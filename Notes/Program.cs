05584d2e3dff9c563a6d43bf0d7e24c8bdfe6989
Austin Ehrmantraut <aehrmantraut@nd.gov>
2024-10-04 16:32:42 -0500

    Themes and Update Note functionality

--- Notes/Program.cs
+++ Notes/Program.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 using System.Data.SQLite;
+using DevExpress.LookAndFeel;
+using DevExpress.XtraEditors;
 
 namespace Notes
 {
@@ -18,46 +20,9 @@ namespace Notes
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            BonusSkins.Register();
+            SkinManager.EnableFormSkins();
             Application.Run(new NewNoteForm());
-
-
-            /**
-             * 
-            string dbPath = "notes.db"; // Specify your database file name
-
-            // Create a new database file
-            using (var connection = new SQLiteConnection($"Data Source={dbPath};Version=3;"))
-            {
-                try
-                {
-                    connection.Open();
-                    Console.WriteLine("Database connection opened.");
-
-                    // Create a new table (example)
-                    string createTableQuery = @"CREATE TABLE IF NOT EXISTS Messages (
-                                                Id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                                Title TEXT NOT NULL,
-                                                Message TEXT NOT NULL,
-                                                Date DATETIME DEFAULT CURRENT_TIMESTAMP
-                                              );";
-
-                    using (var command = new SQLiteCommand(createTableQuery, connection))
-                    {
-                        command.ExecuteNonQuery();
-                        Console.WriteLine("Table 'Messages' created or already exists.");
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"An error occurred: {ex.Message}");
-                }
-                finally
-                {
-                    connection.Close();
-                    Console.WriteLine("Database connection closed.");
-                }
-            }
-             */
         }
     }
 }
